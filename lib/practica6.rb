cf2c59d110f5ef924023381748f4f36d7843ab76
Eduardo Nuez Wehbe <alu0101128153@ull.edu.es>
2019-11-12 23:25:12 +0000

    Creado el initialize donde se inicializaran todas las variables, ademas la comprobacion de que existe un nombre ha sido todo un exito

--- lib/practica6.rb
+++ lib/practica6.rb
@@ -3,5 +3,14 @@ require "practica6/version"
 module Practica6
   class Error < StandardError; end
   class Alimento
+	   attr_reader :nombre, :proteinas, :carbohidratos, :lipidos, :GEI, :terreno
+        def initialize(nombre,proteinas,carbohidratos,lipidos,gei,terreno)
+                @nombre = nombre.to_s
+                @proteinas = proteinas
+                @carbohidratos = carbohidratos
+                @lipidos = lipidos
+                @GEI = gei
+                @terreno = terreno
+        end  
   end
 end
--- spec/practica6_spec.rb
+++ spec/practica6_spec.rb
@@ -4,7 +4,8 @@ require './lib/practica6.rb'
 
 describe Practica6::Alimento do
   it 'crea un objeto tipo alimento' do
-    alimento = Practica6::Alimento.new
+    alimento = Practica6::Alimento.new("carne de vaca",21.1,0.0,3.1,50.0,164.0)
+
     expect(alimento).to be_kind_of(Practica6::Alimento)
   end
 
