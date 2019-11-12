8161b185fe595bac9cc4ae78ede705ecc7911686
Eduardo Nuez Wehbe <alu0101128153@ull.edu.es>
2019-11-12 22:44:17 +0000

    Creando el fichero practica6_spect.rb y haciendo la prueba de verificar si se crea un objeto tipo alimento

---
+++ spec/practica6_spec.rb
@@ -0,0 +1,12 @@
+
+
+
+
+describe Practica6::Alimento do
+  it 'crea un objeto tipo alimento' do
+    alimento = Practica6::Alimento.new
+    expect(alimento).to be_kind_of(Practica6::Alimento)
+  end
+
+end
+
