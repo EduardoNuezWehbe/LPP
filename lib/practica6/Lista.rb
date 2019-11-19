942a4eaf3deb01d3dd774b353472fb3074a0dcac
Eduardo Nuez Wehbe <alu0101128153@ull.edu.es>
2019-11-19 18:06:23 +0000

    Existe un nodo en la clase

--- lib/practica6/Lista.rb
+++ lib/practica6/Lista.rb
@@ -5,9 +5,9 @@ module Practica6
 	Nodo = Struct.new(:value,:next,:prev)
 	class Lista
 		attr_reader :head, :tail
-		def initialize ()
-		 #	@head = Nodo.new(valor,nil,nil)
-		#	@tail = Nodo.new(nil,nil,nil)
+		def initialize (valor)
+			@head = Nodo.new(valor,nil,nil)
+			@tail = Nodo.new(nil,nil,nil)
 		end
 	end
 end
