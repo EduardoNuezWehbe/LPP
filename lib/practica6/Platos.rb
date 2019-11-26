7c8b302d591a92e89d1482dd399d7088d0aea90d
Eduardo Nuez Wehbe <alu0101128153@ull.edu.es>
2019-11-26 20:30:54 +0000

    Metodo para calcular las proteinas de la dieta funcionando

--- lib/practica6/Platos.rb
+++ lib/practica6/Platos.rb
@@ -12,12 +12,15 @@ module Practica6
 			aux = listaAlimentos.head
 			aux2 = listaGramos.head
 			proteinas = 0
+			gramosTotales = 0
 			while aux != nil && listaGramos != nil
 				proteinas += aux.value.proteinas * (aux2.value/100)
+				gramosTotales = aux2.value
 				aux = aux.next
 				aux2 = aux2.next
 			end
-			return proteinas.round(2)
+			return ((proteinas * 100)/gramosTotales).round(2)
 		end
+
 	end
 end
